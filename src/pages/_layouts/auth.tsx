c04348c0090038df874ef1791636a670aa921b49
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-03-25 20:52:12 -0300

    Feat: start create header

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -4,7 +4,7 @@ import image from "../../assets/_fa5ac735-2516-4842-8bd7-b49435f90d67.jpeg";
 
 export function AuthLayout() {
     return(
-        <div className="grid min-h-screen grid-cols-2">
+        <div className="grid min-h-screen grid-cols-2 antialiased">
             <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
                 <div className="flex items-center gap-3 text-lg text-foreground">
                     <img src={image} alt=""/>
